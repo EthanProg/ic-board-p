07760117d9446710abdc5d4d60fbd4cf5899cc67
CharlesSong <Charles2Song@gmail.com>
2015-04-16 14:39:31 +0800

    配置更改

--- src/main/java/com/ic/manager/user/controller/UserController.java
+++ src/main/java/com/ic/manager/user/controller/UserController.java
@@ -2,6 +2,9 @@ package com.ic.manager.user.controller;
 
 import com.ic.core.enums.LogLevel;
 import com.ic.core.impl.CoreController;
+import com.ic.core.model.CodeModel;
+import com.ic.core.page.Page;
+import com.ic.core.utils.JSONUtil;
 import com.ic.manager.user.model.User;
 import com.ic.manager.user.service.IUserService;
 import net.sf.json.JSONArray;
@@ -24,7 +27,7 @@ import java.util.Map;
  * Date  : 2014/12/13
  */
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 public class UserController extends CoreController {
 
     private IUserService userService;
@@ -33,6 +36,29 @@ public class UserController extends CoreController {
     @Autowired
     public void setUserService(IUserService userService) {
         this.userService = userService;
+    }
+
+    @RequestMapping("/{currentPage}/{pageSize}")
+    public @ResponseBody Map<String,Object> userListPage(@PathVariable int currentPage,@PathVariable int pageSize){
+        super.execute();
+        Page page = new Page();
+        if(currentPage>1){
+            page.setCurrentPage(currentPage);
+        }else {
+            page.setCurrentPage(1);
+        }
+        if (pageSize==0){
+            page.setPageSize(10);
+        }else{
+            page.setPageSize(pageSize);
+        }
+        page.setPagination(true);
+        params.put("page", page);
+        List<Map<String,Object>> userList=userService.queryUserList(params);
+
+        returnMap.put("userList",userList);
+        returnMap.put("page",JSONObject.fromObject(page));
+        return returnMap;
     }
 
     /**
@@ -53,14 +79,52 @@ public class UserController extends CoreController {
      */
     @RequestMapping("/{userId}")
     public @ResponseBody Map<String,Object> user(@PathVariable String userId){
-        Map<String,Object> params=new HashMap<String,Object>();
-        params.put("userId",userId);
+        params.put("user_id",userId);
         System.out.println("用户id："+userId);
         return userService.queryUser(params);
     }
 
+    /**
+     * 功能：用户添加。inJson=按照表中字段组织的json字符串
+     * @param req
+     * @return
+     */
     @RequestMapping("/add")
-    public String add(){
-        return null;
+    public Map<String,Object> add(HttpServletRequest req){
+        String inJson=req.getParameter("inJson");
+        if(inJson!=null && !inJson.equals("")){
+            params= JSONUtil.parseJSONString2Map(inJson);
+            returnMap=userService.addUser(params);
+        }else{
+            returnMap.put("code", CodeModel.CODE_EMPTY_FAILURE);
+            returnMap.put("msg", "参数为空");
+        }
+        return returnMap;
+    }
+
+    /**
+     *
+     * @param userId
+     * @return
+     */
+    @RequestMapping("/del/{userId}")
+    public @ResponseBody Map<String,Object> delete(@PathVariable String userId){
+        super.execute();
+        params.put("user_id",userId);
+        Map<String,Object> rt=userService.deleteUser(params);
+        return rt;
+    }
+
+    @RequestMapping("/update")
+    public Map<String,Object> update(HttpServletRequest req){
+        String inJson=req.getParameter("inJson");
+        if(inJson!=null && !inJson.equals("")){
+            params= JSONUtil.parseJSONString2Map(inJson);
+            returnMap=userService.updateUser(params);
+        }else{
+            returnMap.put("code", CodeModel.CODE_EMPTY_FAILURE);
+            returnMap.put("msg", "参数为空");
+        }
+        return returnMap;
     }
 }
--- src/main/java/com/ic/manager/user/service/UserServiceImpl.java
+++ src/main/java/com/ic/manager/user/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ic.manager.user.service;
 
-import java.util.List;
-import java.util.Map;
-
+import com.ic.core.model.CodeModel;
+import com.ic.core.utils.IdGenerate;
 import com.ic.manager.user.mapper.UserServiceMapper;
-import com.ic.manager.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Description:用户服务接口实现类
@@ -15,8 +17,13 @@ import org.springframework.stereotype.Service;
  */
 @Service("userService")
 public class UserServiceImpl implements IUserService {
-	@Autowired
+
 	private UserServiceMapper userServiceMapper;
+
+	@Autowired
+	public void setUserServiceMapper(UserServiceMapper userServiceMapper) {
+		this.userServiceMapper = userServiceMapper;
+	}
 
 	@Override
 	public List<Map<String,Object>> queryUserList(Map<String, Object> params) {
@@ -26,5 +33,48 @@ public class UserServiceImpl implements IUserService {
 	@Override
 	public Map<String,Object> queryUser(Map<String, Object> params) {
 		return userServiceMapper.queryUser(params);
+	}
+
+
+	@Override
+	public Map<String, Object> addUser(Map<String, Object> params) {
+		params.put("user_id", IdGenerate.nextId());
+		int nums= userServiceMapper.addUser(params);
+		Map<String,Object> rt=new HashMap<String, Object>();
+		if(nums>0){
+			rt.put("code", CodeModel.CODE_SUCCESS);
+			rt.put("msg","新增用户成功");
+		}else{
+			//如何提示？
+		}
+		return rt;
+	}
+
+	@Override
+	public Map<String, Object> deleteUser(Map<String, Object> params) {
+		int nums= userServiceMapper.deleteUser(params);
+		Map<String,Object> rt=new HashMap<String, Object>();
+		if(nums>0){
+			rt.put("code", CodeModel.CODE_SUCCESS);
+			rt.put("msg","删除用户成功");
+		}else{
+			rt.put("code", CodeModel.CODE_SUCCESS_EMPTY);
+			rt.put("msg","无此用户");
+		}
+		return rt;
+	}
+
+	@Override
+	public Map<String, Object> updateUser(Map<String, Object> params) {
+		int nums= userServiceMapper.updateUser(params);
+		Map<String,Object> rt=new HashMap<String, Object>();
+		if(nums>0){
+			rt.put("code", CodeModel.CODE_SUCCESS);
+			rt.put("msg","更新用户成功");
+		}else{
+			rt.put("code", CodeModel.CODE_SUCCESS_EMPTY);
+			rt.put("msg","无此用户");
+		}
+		return rt;
 	}
 }
