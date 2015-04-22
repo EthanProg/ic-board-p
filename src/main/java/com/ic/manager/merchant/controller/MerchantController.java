bd17af695c833b1e866b7be0390cec21eb64021c
xjw <239386839@qq.com>
2015-04-22 22:37:28 +0800

    xjw

---
+++ src/main/java/com/ic/manager/merchant/controller/MerchantController.java
@@ -0,0 +1,9 @@
+package com.ic.manager.merchant.controller;
+
+/**
+ * Created by xjw on 2015/4/22.
+ */
+public class MerchantController
+{
+
+}
---
+++ src/main/java/com/ic/manager/merchant/model/Merchant.java
@@ -0,0 +1,15 @@
+package com.ic.manager.merchant.model;
+
+/**
+ * Created by xjw on 2015/4/22.
+ */
+public class Merchant
+{
+    private String merchant_id;
+    private String merchant_name;
+    private String merchant_short_name;
+    private String tell_num;
+    private String passWord;
+
+
+}
