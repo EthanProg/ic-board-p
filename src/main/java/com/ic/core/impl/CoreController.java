ceb607abcaa9a704da96e7baf1300713fb83d451
xjw <239386839@qq.com>
2015-04-26 21:11:54 +0800

    添加统一处理前台请求的方法，获取每次求请的request和response对象

--- src/main/java/com/ic/core/impl/CoreController.java
+++ src/main/java/com/ic/core/impl/CoreController.java
@@ -4,10 +4,12 @@ package com.ic.core.impl;
 import com.ic.core.enums.LogLevel;
 import com.ic.core.mapper.JsonMapper;
 import com.ic.core.model.DataResponse;
+import com.ic.core.utils.JSONUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.ContextLoader;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +33,24 @@ public abstract class CoreController implements Serializable {
      */
     protected Logger log = LoggerFactory.getLogger(getClass());
 
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+    protected Map<String,Object> param_map;
+
+    /**
+     * 执行任意controllor首先执行此方法，用来统一处理前台请求参数
+     * @param request
+     * @param response
+     */
+    @ModelAttribute
+    protected void preparedParam(HttpServletRequest request, HttpServletResponse response)
+    {
+        this.request = request;
+        this.response = response;
+        String inJson = request.getParameter("inJson");
+        param_map = JSONUtil.parseJSONString2Map(inJson);
+        System.out.println("首先执行此方法，用来统一处理前台请求参数");
+    }
 
     public void debug(String string){
         if(log.isDebugEnabled()){
@@ -53,29 +73,27 @@ public abstract class CoreController implements Serializable {
     /**
      * 客户端返回JSON字符串
      *
-     * @param response
      * @param object
      * @return
      */
-    protected String renderString(HttpServletResponse response, Object object) {
-        return renderString(response, JsonMapper.toJsonString(object), "application/json");
+    protected String renderString(Object object) {
+        return renderString(JsonMapper.toJsonString(object), "application/json");
     }
 
 
-    protected String renderData(HttpServletResponse response,String code,String msg, Map<String,Object> data) {
+    protected String renderData(String code,String msg, Map<String,Object> data) {
         DataResponse dr=new DataResponse(code,msg,data);
         dr.setTranstime(null);
-        return renderString(response, JsonMapper.toJsonString(dr), "application/json");
+        return renderString(JsonMapper.toJsonString(dr), "application/json");
     }
 
     /**
      * 客户端返回字符串
      *
-     * @param response
      * @param string
      * @return
      */
-    protected String renderString(HttpServletResponse response, String string, String type) {
+    protected String renderString(String string, String type) {
         try {
             response.reset();
             response.setContentType(type);
