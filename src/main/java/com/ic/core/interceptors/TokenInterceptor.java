9281a988ee24593e05ea15ab0bb706d59d9163f9
CharlesSong <Charles2Song@gmail.com>
2015-04-27 18:16:20 +0800

    日志改为按天打印

---
+++ src/main/java/com/ic/core/interceptors/TokenInterceptor.java
@@ -0,0 +1,34 @@
+package com.ic.core.interceptors;
+
+import com.ic.core.utils.CacheUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Description:
+ * Author: CharlesSong
+ * Date  : 2015/4/26
+ */
+public class TokenInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("interceptor preHandle");
+        System.out.println(o);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("interceptor postHandle");
+        System.out.println((Map<String, Object>) CacheUtils.get("userCache", "user"));
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("interceptor postHandle");
+    }
+}
--- src/main/java/com/ic/modules/user/controller/UserCtrl.java
+++ src/main/java/com/ic/modules/user/controller/UserCtrl.java
@@ -1,8 +1,16 @@
 package com.ic.modules.user.controller;
 
-import com.ic.core.impl.CoreController;
+import com.ic.core.base.CoreController;
+import com.ic.core.interceptors.TokenInterceptor;
+import com.ic.core.model.Code;
+import com.ic.core.utils.CacheUtils;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Description:
@@ -12,9 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 public class UserCtrl extends CoreController {
-    @RequestMapping("/list")
-    public void list(){
+
+    public void register(){
+
+    }
+
+    @RequestMapping("/test")
+    public void list(HttpServletResponse resp){
         System.out.println("这是user list");
-        debug("这是user list ");
+
+        Map<String,Object> data=new HashMap<String,Object>();
+        data.put("user","123456");
+        CacheUtils.put("userCache","user", data);
+        renderData(resp, Code.SUCCESS, Code.SUCCESS_MSG, data);
     }
 }
