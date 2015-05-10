24a6b2984ce8f752996d4cb0c2148163168c4961
CharlesSong <Charles2Song@gmail.com>
2015-05-10 13:58:58 +0800

    用户登录，注册，校验

--- src/main/java/com/ic/modules/user/controller/UserCtrl.java
+++ src/main/java/com/ic/modules/user/controller/UserCtrl.java
@@ -1,14 +1,18 @@
 package com.ic.modules.user.controller;
 
 import com.ic.core.base.CoreController;
-import com.ic.core.interceptors.TokenInterceptor;
+import com.ic.core.config.Global;
+import com.ic.core.mapper.JsonMapper;
 import com.ic.core.model.Code;
-import com.ic.core.utils.CacheUtils;
-import org.aspectj.lang.annotation.Before;
+import com.ic.core.utils.MD5Util;
+import com.ic.modules.user.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,17 +25,61 @@ import java.util.Map;
 @RequestMapping("/api/user")
 public class UserCtrl extends CoreController {
 
-    public void register(){
+    @Autowired
+    private UserServiceImpl userService;
+
+    @RequestMapping("/login")
+    public void login(HttpServletRequest req, HttpServletResponse resp, HttpSession session){
+        Map<String, Object> param = (Map<String, Object>) req.getAttribute("param");
+        Map<String, Object> user = userService.getUser(param);
+        if(user== null){
+            renderData(Code.FAIL, "用户不存在", null);
+        }else{
+            if(!user.get("user_pwd").equals(MD5Util.getMD5String(Global.getConfig("md5salt")+param.get("userPwd")))){
+                renderData(Code.FAIL, "用户名/密码错误", null);
+            }else{
+                renderData(Code.SUCCESS,"登录成功",user);
+            }
+        }
+    }
+
+    @RequestMapping("/reg")
+    public void register(HttpServletRequest req,HttpServletResponse resp,HttpSession session){
+        Map<String, Object> param = (Map<String, Object>) req.getAttribute("param");
+        if(param.get("userName")==null || param.get("userPwd") == null) {
+            renderData(Code.PARAMS_ERROR, "请输入用户名/密码", null);
+        }else{
+            Map<String, Object> out = userService.getUser(param);
+            String exists = (String) session.getAttribute("exists");
+            if ("1".equals(exists) || out != null) {
+                renderData(Code.FAIL, "用户已经存在，请不要重复注册", null);
+            } else {
+                int rtn = userService.insertUser(param);
+                if (rtn == 1) {
+                    renderData(Code.SUCCESS, "用户注册成功", null);
+                } else {
+                    renderData(Code.FAIL, "用户注册失败", null);
+                }
+            }
+        }
 
     }
 
-    @RequestMapping("/test")
-    public void list(HttpServletResponse resp){
-        System.out.println("这是user list");
-
-        Map<String,Object> data=new HashMap<String,Object>();
-        data.put("user","123456");
-        CacheUtils.put("userCache","user", data);
-        renderData(resp, Code.SUCCESS, Code.SUCCESS_MSG, data);
+    @RequestMapping("/check")
+    public void check(HttpServletRequest req,HttpServletResponse resp,HttpSession session){
+        Map<String, Object> param = (Map<String, Object>) req.getAttribute("param");
+        if(param.get("userName")==null){
+            renderData(Code.PARAMS_ERROR, "请输入用户名", null);
+        }else{
+            Map<String, Object> out = userService.getUser(param);
+            if (out == null) {
+                out = new HashMap<String, Object>();
+                out.put("exists", "0");
+            } else {
+                out = new HashMap<String, Object>();
+                out.put("exists", "1");
+            }
+            renderData(Code.SUCCESS, "校验用户是否存在成功", out);
+        }
     }
 }
